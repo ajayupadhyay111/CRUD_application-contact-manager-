3cae6f5b3e6e70121f4764536172b76a76ae6160
ajayupadhyay111 <upadhyayajay156@gmail.com>
2025-02-27 10:43:28 +0530

    file updated

--- frontend/src/apiCalls/index.js
+++ frontend/src/apiCalls/index.js
@@ -2,11 +2,14 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:4000";
 let newData = null;
-export const GetAllContacts = async (setContacts) => {
+export const GetAllContacts = async (setContacts,setFilteredContacts) => {
   try {
     fetch(`${baseUrl}/allContacts`)
       .then((res) => res.json())
-      .then((data) => setContacts(data))
+      .then((data) => {
+        setContacts(data)
+      setFilteredContacts(data)
+      })
       .catch((err) => console.log(err));
   } catch (error) {
     console.log(error.message);
@@ -14,30 +17,30 @@ export const GetAllContacts = async (setContacts) => {
   }
 };
 
-export const newContact = async (setContacts, formData) => {
+export const newContact = async (setFilteredContacts, formData) => {
   try {
     let response = await axios.post(`${baseUrl}/newContact`, formData);
     console.log(response)
-    setContacts((prev) => [...prev, response.data.user]);
+    setFilteredContacts((prev) => [...prev, response.data.user]);
   } catch (error) {
     alert(error.response.data.message);
   }
 };
-export const updateContact = async (setContacts, contact) => {
+export const updateContact = async (setFilteredContacts, contact) => {
   try {
     let response = await axios.put(`${baseUrl}/updateContact/${contact.id}`,contact);
 
-    setContacts(prev=>prev.map(item=>item.id === response.data.data.id ? response.data.data : item))
+    setFilteredContacts(prev=>prev.map(item=>item.id === response.data.data.id ? response.data.data : item))
     
   } catch (error) {
     console.log(error.message || error.response.data.message);
   }
 };
 
-export const deleteContact = async (setContacts, id) => {
+export const deleteContact = async (setFilteredContacts, id) => {
   try {
     const response = await axios.delete(`${baseUrl}/deleteContact/${id}`);
-    setContacts(prev=>prev.filter(item=>item.id !== response.data.contact.id))
+    setFilteredContacts(prev=>prev.filter(item=>item.id !== response.data.contact.id))
   } catch (error) {
     console.log(error.message || error.response.data.message);
   }
